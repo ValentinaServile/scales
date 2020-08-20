5ae9455153151987915fb9ab8e62126f5de3262d
Valentina Servile <valentina.servile@gmail.com>
2020-08-20 14:11:33 +0200

    Add builds to .gitignore

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ echo "Creating archive with version $VERSION..."
 
 tar \
   --exclude="./src/*_spec.rb" \
-  --exclude="./archives" \
   --exclude="*.sh" \
   --exclude=".idea" \
   --exclude=".gitignore" \
   --exclude=".ruby-version" \
   --exclude=".git" \
+  --exclude="*.tar.gz" \
   -czvf \
-  "archives/scales-$VERSION.tar.gz" \
+  "scales-$VERSION.tar.gz" \
   .
